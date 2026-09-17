a086b635dd1205628e7b44ff948a9cc924536fba
Nilesh Yadav <ny7531183@gmail.com>
2026-09-17 13:05:17 +0530

    basic set up

--- app.py
+++ app.py
@@ -2,3 +2,12 @@
 #command .\env\Scripts\activate
 
 # python -m pip install -r requirements.txt
+
+
+import streamlit as st
+import os 
+
+# st.set_page_config(page_title="Health Assistant",page_icon="😊",layout="wide")
+
+st.title("AI HEALTH ASSISTANT 😊")
+st.header("Health Information")
