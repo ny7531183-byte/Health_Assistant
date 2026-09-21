e67cf2e0a33f71bbae70c076b0909912ceac09d0
Nilesh Yadav <ny7531183@gmail.com>
2026-09-21 12:58:06 +0530

    Update Project

--- app.py
+++ app.py
@@ -6,8 +6,40 @@
 
 import streamlit as st
 import os 
+from diet import bmi_calculator,bmr_calculator,tdee_calculator,calorie_target #here we import formula
 
 # st.set_page_config(page_title="Health Assistant",page_icon="😊",layout="wide")
 
 st.title("AI HEALTH ASSISTANT 😊")
+
+st.write("Personal Health Assistance and Diet Recommendation Agent")
 st.header("Health Information")
+
+st.sidebar.header("🤷‍♂️Your Information")
+##--------------------------------------------------------------------- for controls##
+
+gender=st.sidebar.selectbox("Gender",['Male','Female'])
+age=st.sidebar.number_input("Age",1,100)
+weight=st.sidebar.number_input("Weight(kg)",1,120)
+height=st.sidebar.number_input("Height(cm)",100,200)
+activity=st.sidebar.selectbox("Activity",["Sedentary",
+                                          "Lightly Active",
+                                          "Moderately Active",
+                                          "Very Active",
+                                          "Extra Active"])
+aim=st.sidebar.selectbox("AIM",["weight maintain","weight loss","weight gain"])
+
+##----------------------------------------------------------------------------------------------###
+
+bmi=bmi_calculator(weight,height)
+bmr=bmr_calculator(gender,age,weight,height)                       #function call here
+tdee=tdee_calculator(bmr,activity)
+calorie=calorie_target(tdee,aim)
+
+##----------------------------------------------------------------------------------------------------##
+
+col1,col2,col3,col4=st.columns(4)
+col1.metric("BMI",bmi)                               ##information show on interface by this all code
+col2.metric("BMR",f"{bmr} Kcal")
+col3.metric("TDEE",f"{tdee} Kcal")
+col4.metric("Calorie Target",f"{calorie} Kcal")
--- diet.py
+++ diet.py
@@ -3,10 +3,10 @@ def bmi_calculator(weight,height):
     return round (bmi,2)
 
 def bmr_calculator(gender,age,weight,height):
-    if gender=="male":
+    if gender=="Male":
         bmr=(10*weight)+(6.25*height)-(5*age)+5
         return bmr
-    elif gender=="female":
+    elif gender=="Female":
          bmr=(10*weight)+(6.25*height)-(5*age)-161
          return bmr
 
